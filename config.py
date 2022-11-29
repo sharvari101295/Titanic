6dd63e9ec0dc97dd365e0128f6bb9b8d5aa7d165
sharvari shinde <mail2sharvarishinde@gmail.com>
2022-11-29 15:31:27 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+PORT_NUMBER = 8080
+
+#MODEL_FILE_PATH = os.path.join("D:\VLCNotes\Model_Building\Titanic_EndToEnd\project_app\DT_Classifier.pkl")
+#JSON_FILE_PATH = os.path.join("D:\VLCNotes\Model_Building\Titanic_EndToEnd\project_app\project_data.json")
+
+MODEL_FILE_PATH = os.path.join("project_app","DT_Classifier.pkl")
+JSON_FILE_PATH = os.path.join("project_app","project_data.json")
